87ed0cef8d27729ce5edd151308223983d1a65c8
Georgi Slaveykov <slaweykow@protonmail.com>
2019-01-29 06:53:43 +0200

    Add an embed for errors (sendErrorEmbed)

--- src/misc/embeds.ts
+++ src/misc/embeds.ts
@@ -15,6 +15,7 @@ import { Message } from 'discord.js';
  * @param content - The embed's content. (uses 'embed.description')
  * @param color - The embed's color
  * @returns Promise '<Message | Message[]>' object
+ * @example sendEmbed(msg, `This is a title`, 'This is the embed's content', 0xf2a15a);
  */
 export function sendEmbed(message: CommandMessage, title: string, content: string, color: number) {
 		const promise: Promise<Message | Message[]> = message.embed({
@@ -28,4 +29,16 @@ export function sendEmbed(message: CommandMessage, title: string, content: strin
 		});
 
 		return promise;
+}
+
+/**
+ * Send an error embed
+ * @param message - Message to use as promise base
+ * @param error - The error's message
+ * @param color - The embed's color
+ * @returns function - sendEmbed(message, error, color);
+ * @example sendErrorEmbed(msg, 'Error message', 0xFF0000);
+ */
+export function sendErrorEmbed(message: CommandMessage, error: string, color: number) {
+	return sendEmbed(message, 'An error has occured!', `❌ **Error:**  ${error} `, color);
 }
